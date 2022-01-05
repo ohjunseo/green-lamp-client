86a422298de47b61798d8ba543fea074eb75bf9c
kristy43924 <kristy43924@gmail.com>
2022-01-05 16:47:07 +0900

    웹페이지

---
+++ src/detailView/index.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./detail.scss";
+function ProductView(){
+    const [ product, setProduct ] = useState(null);
+    const param = useParams();
+    const { id } = param;
+    useEffect(()=>{
+        axios.get(
+            `https://3dcecb53-56ec-4d0c-8fc4-26e6c7d7dcdf.mock.pstmn.io/products/${id}`
+        ).then(function(result){
+            setProduct(result.data);
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    },[])
+    if(product == null){
+        return <div>상품정보를 받고있습니다.....</div>
+    }
+    return(
+        <div className="innerCon" id="detail">
+            <h1>{product.name}</h1>
+            <div id="image-box">
+                <img src={product.img} alt="제품" />
+            </div>
+            <div id="profile-box">
+                <img src="/images/icons/avatar.png" alt="아이콘" />
+                <span>{product.seller}</span>
+            </div>
+            <div id="contents-box">
+                <div>{product.name}</div>
+                <div>{product.price}</div>
+                <div>2022년 1월 4일</div>
+                <div>설명글</div>
+            </div>
+        </div>
+    );
+}
+export default ProductView;
